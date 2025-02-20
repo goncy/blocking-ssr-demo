23f58ad115c57f5b41cb8ef63ac08fb3eea166d1
goncy <gonzalo.pozzo4@gmail.com>
2025-02-20 16:06:36 -0300

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 
 import {Suspense} from "react";
+import Link from "next/link";
 
 import "../globals.css";
 
@@ -10,5 +11,17 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({children}: {children: React.ReactNode}) {
-  return <Suspense>{children}</Suspense>;
+  return (
+    <Suspense>
+      <html lang="en">
+        <body className="container m-auto grid min-h-screen grid-rows-[auto_1fr_auto] gap-8 px-4 font-sans antialiased">
+          <header className="text-xl leading-[4rem] font-bold">
+            <Link href="/">Next Store</Link>
+          </header>
+          {children}
+          <footer className="text-center leading-[4rem] opacity-70">Next Store</footer>
+        </body>
+      </html>
+    </Suspense>
+  );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export interface Product {
-  id: number
-  name: string
-  price: number
-  image: string
+  id: number;
+  name: string;
+  price: number;
+  image: string;
 }
